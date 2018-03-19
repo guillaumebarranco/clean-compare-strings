6351fdb74adab87ab3cce3b5cf9eaf0766165bac
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2018-03-19 15:47:34 +0100

    write in ts

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-export function cleanString(text) {
-	return text
-		.toLowerCase()
-	    .replace(/-/g, ' ')
-	    .normalize('NFD').replace(/[\u0300-\u036f]/g, "") // remove accents
-	    .replace(/!/g, '')
-	    .replace(/\?/g, '')
-	    .trim();
+"use strict";
+exports.__esModule = true;
+/**
+ * Converted to string only
+ * No uppercase
+ * No hyphen nor underscore
+ * No accent
+ * No punctuation
+ * No spaces around
+ */
+function cleanString(text) {
+    return text
+        .toString()
+        .toLowerCase()
+        .replace(/-/g, ' ')
+        .replace(/_/g, ' ')
+        .normalize('NFD').replace(/[\u0300-\u036f]/g, "") // remove accents
+        .replace(/!/g, '')
+        .replace(/\?/g, '')
+        .trim();
 }
-
-export const stringsMatch = (str1, str2) => clean(str1) === clean(str2);
+exports.cleanString = cleanString;
+exports.stringsMatch = function (str1, str2) { return cleanString(str1) === cleanString(str2); };
---
+++ index.ts
@@ -0,0 +1,23 @@
+type StringOrNumber = string | number;
+
+/**
+ * Converted to string only
+ * No uppercase
+ * No hyphen nor underscore
+ * No accent
+ * No punctuation
+ * No spaces around
+ */
+export function cleanString(text: StringOrNumber): string {
+	return text
+		.toString()
+		.toLowerCase()
+	    .replace(/-/g, ' ')
+	    .replace(/_/g, ' ')
+	    .normalize('NFD').replace(/[\u0300-\u036f]/g, "") // remove accents
+	    .replace(/!/g, '')
+	    .replace(/\?/g, '')
+	    .trim();
+}
+
+export const stringsMatch = (str1: StringOrNumber, str2: StringOrNumber) => cleanString(str1) === cleanString(str2);
